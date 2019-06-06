7ec0d34d0e2803c6581d0a5912a714c0f56990b3
Vlad Dotsenko <karponter.tarino@gmail.com>
2019-06-07 01:25:35 +0300

    feat. implement .add() method

--- RangeList.js
+++ RangeList.js
@@ -1,17 +1,77 @@
 'use strict';
 
-/**
- *
- * NOTE: Feel free to add any extra member variables/functions you like.
- */
+// A class named 'RangeList'
+// A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
+// A range list is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
 
 class RangeList {
+  static _validateRangeInput(range) {
+    if (!Array.isArray(range))
+      throw new TypeError(`Invalid input range: expected array, got ${typeof range}`)
+
+    const [from = 0, to = 0] = range;
+    if (to < from)
+      throw new TypeError(`Invalid input range, ${range}`);
+  }
+
+  /**
+   * Compare ranges A and B
+   * Return:
+   *    1 if B > A
+   *    0 if B collides A
+   *   -1 if B < A
+   */
+  static _compareRanges(rangeA, rangeB) {
+    if (rangeA[1] < rangeB[0]) return 1;
+    if (rangeA[0] > rangeB[1]) return -1;
+    return 0;
+  }
+
+  static _mergeRanges(rangeA, rangeB) {
+    return [Math.min(rangeA[0], rangeB[0]), Math.max(rangeA[1], rangeB[1])];
+  }
+
+  static _textualRelation(relation) {
+    if (relation === -1) return '>';
+    if (relation === 0) return 'X';
+    if (relation === 1) return '<';
+  }
+
+  constructor({ logger = console } = {}) {
+    this.logger = logger;
+    this.ranges = [];
+  }
+
   /**
    * Adds a range to the list
    * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
    */
-  add(range) {
-    // TODO: implement this
+  add(range = []) {
+    RangeList._validateRangeInput(range);
+    const updatedRanges = [];
+    let rest = [];
+    let candidate = range;
+
+    for (let i = 0; i < this.ranges.length; i++) {
+      const targetRange = this.ranges[i];
+      const rangesRelation = RangeList._compareRanges(candidate, targetRange);
+      // console.log(`-- rangesRelation for ${targetRange} is ${RangeList._textualRelation(rangesRelation)}`);
+      if (rangesRelation === -1) {
+        updatedRanges.push(targetRange);
+        continue;
+      }
+      if (rangesRelation === 0) {
+        candidate = RangeList._mergeRanges(candidate, targetRange);
+        continue;
+      }
+      if (rangesRelation === 1) {
+        rest = this.ranges.slice(i);
+        break;
+      }
+    }
+
+    updatedRanges.push(candidate);
+    this.ranges = updatedRanges.concat(rest);
   }
 
   /**
@@ -19,6 +79,7 @@ class RangeList {
    * @param {Array<number>} range - Array of two integers that specify beginning and end of range.
    */
   remove(range) {
+    RangeList._validateRangeInput(range);
     // TODO: implement this
   }
 
@@ -26,6 +87,12 @@ class RangeList {
    * Prints out the list of ranges in the range list
    */
   print() {
-    // TODO: implement this
+    const textualRanges = this.ranges.map(([a, b]) => `[${a}, ${b})`);
+    const message = textualRanges.join(' ');
+    this.logger && this.logger.info(message);
+
+    return message;
   }
 }
+
+module.exports = RangeList;
--- test.js
+++ test.js
@@ -1,48 +1,79 @@
-// Task: Implement a class named 'RangeList'
-// A pair of integers define a range, for example: [1, 5). This range includes integers: 1, 2, 3, and 4.
-// A range list is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
+'use strict';
 
+const assert = require('assert');
 const RangeList = require('./RangeList');
 
-// Example run
-const rl = new RangeList();
+describe('RangeList class', () => {
+  const rl = new RangeList({ logger: null });
 
-rl.add([1, 5]);
-rl.print();
-// Should display: [1, 5)
+  it('.add([1, 5]), should display: [1, 5)', () => {
+    rl.add([1, 5]);
+    const expectableText = '[1, 5)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.add([10, 20]);
-rl.print();
-// Should display: [1, 5) [10, 20)
+  it('.add([10, 20]), should display: [1, 5) [10, 20)', () => {
+    rl.add([10, 20]);
+    const expectableText = '[1, 5) [10, 20)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.add([20, 20]);
-rl.print();
-// Should display: [1, 5) [10, 20)
+  it('.add([20, 20]), should display: [1, 5) [10, 20)', () => {
+    rl.add([20, 20]);
+    const expectableText = '[1, 5) [10, 20)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.add([20, 21]);
-rl.print();
-// Should display: [1, 5) [10, 21)
+  it('.add([20, 21]), should display: [1, 5) [10, 21)', () => {
+    rl.add([20, 21]);
+    const expectableText = '[1, 5) [10, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.add([2, 4]);
-rl.print();
-// Should display: [1, 5) [10, 21)
+  it('.add([2, 4]), should display: [1, 5) [10, 21)', () => {
+    rl.add([2, 4]);
+    const expectableText = '[1, 5) [10, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.add([3, 8]);
-rl.print();
-// Should display: [1, 8) [10, 21)
+  it('.add([3, 8]), should display: [1, 8) [10, 21)', () => {
+    rl.add([3, 8]);
+    const expectableText = '[1, 8) [10, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.remove([10, 10]);
-rl.print();
-// Should display: [1, 8) [10, 21)
+  it('.remove([10, 10]), should display: [1, 8) [10, 21)', () => {
+    rl.remove([10, 10]);
+    const expectableText = '[1, 8) [10, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.remove([10, 11]);
-rl.print();
-// Should display: [1, 8) [11, 21)
+  it('.remove([10, 11]), should display: [1, 8) [11, 21)', () => {
+    rl.remove([10, 11]);
+    const expectableText = '[1, 8) [11, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.remove([15, 17]);
-rl.print();
-// Should display: [1, 8) [11, 15) [17, 21)
+  it('.remove([15, 17]), should display: [1, 8) [11, 15) [17, 21)', () => {
+    rl.remove([15, 17]);
+    const expectableText = '[1, 8) [11, 15) [17, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
 
-rl.remove([3, 19]);
-rl.print();
-// Should display: [1, 3) [19, 21)
+  it('.remove([3, 19]), should display: [1, 3) [19, 21)', () => {
+    rl.remove([3, 19]);
+    const expectableText = '[1, 3) [19, 21)';
+    const text = rl.print();
+    assert.strictEqual(text, expectableText);
+  });
+
+});
